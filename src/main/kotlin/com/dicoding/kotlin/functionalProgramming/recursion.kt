36a2cb620c0a2ae3a03e45897bd6a102e401766e
Arief <ariefnhidayah@gmail.com>
2020-05-27 11:35:05 +0700

    update

---
+++ src/main/kotlin/com/dicoding/kotlin/functionalProgramming/recursion.kt
@@ -0,0 +1,23 @@
+package com.dicoding.kotlin.functionalProgramming
+
+fun main() {
+    println("Factorial 10 is: ${factorial(10)}")
+}
+
+//fun factorial(n: Int): Int {
+//    return if (n == 1) {
+//        n
+//    } else {
+//        n * factorial(n - 1)
+//    }
+//}
+
+//menggunakan tail call recursion
+tailrec fun factorial(n: Int, result: Int = 1): Int {
+    val newResult = n * result
+    return if (n == 1) {
+        newResult
+    } else {
+        factorial(n - 1, newResult)
+    }
+}
