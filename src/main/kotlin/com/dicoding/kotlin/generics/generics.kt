fbaf8506a01500b274083c23c31a3729976fc1a1
Arief <ariefnhidayah@gmail.com>
2020-05-30 18:55:11 +0700

    update

---
+++ src/main/kotlin/com/dicoding/kotlin/generics/generics.kt
@@ -0,0 +1,27 @@
+package com.dicoding.kotlin.generics
+
+fun main() {
+//    val generics = listOf("Test", "Ini", "Arief")
+//    println(generics)
+    val longArrayList = ArrayList<Long>()
+    val firstLong = longArrayList.get(0)
+    println(firstLong)
+}
+
+//mendeklarasikan class generics
+interface List<T> {
+    operator fun get(index: Int): T
+}
+
+//contoh menggunakan inheritance
+class LongList: List<Long> {
+    override fun get(index: Int): Long {
+        return 0
+    }
+}
+
+class ArrayList<T>: List<T> {
+    override fun get(index: Int): T {
+        /* ... */
+    }
+}
